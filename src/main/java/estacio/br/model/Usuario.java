ff0e1b6fa9be48e2ed54e0e55a54b11b35b82bdd
Gabriel Mattos <gcostadev@gmail.com>
2025-05-09 16:20:11 -0300

    Model de usuario ajustado

--- src/main/java/estacio/br/model/Usuario.java
+++ src/main/java/estacio/br/model/Usuario.java
@@ -4,12 +4,47 @@ public class Usuario {
     private int id;
     private String nome;
     private String email;
+    private String senha;
+    private int id_tipo_usuario;
 
     // Getters e Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public int getId_tipo_usuario() {
+        return id_tipo_usuario;
+    }
+
+    public void setId_tipo_usuario(int id_tipo_usuario) {
+        this.id_tipo_usuario = id_tipo_usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
