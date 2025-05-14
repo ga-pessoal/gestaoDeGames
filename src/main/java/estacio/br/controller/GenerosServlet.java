6c6892d12a8dc35ad866ce1c432e9690208b2376
Gabriel Mattos <gcostadev@gmail.com>
2025-05-14 15:06:14 -0300

    Crirei a parte de cadatro de genero para editar e excluir; e no GeneroDAO

--- src/main/java/estacio/br/controller/GenerosServlet.java
+++ src/main/java/estacio/br/controller/GenerosServlet.java
@@ -1,11 +1,14 @@
 package estacio.br.controller;
 
+import estacio.br.dao.GeneroDAO;
+import estacio.br.model.Genero;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 public class GenerosServlet extends HttpServlet {
     @Override
@@ -18,10 +21,36 @@ public class GenerosServlet extends HttpServlet {
             // Exibe o formulário de cadastro
             request.setAttribute("pageTitle", "Cadastro de Gênero");
             request.getRequestDispatcher("/jsp/cadastroGenero.jsp").forward(request, response);
+
+        } else if("editar".equals(action)){
+        int id_genero = Integer.parseInt(request.getParameter("id"));
+
+        GeneroDAO dao = new GeneroDAO();
+        Genero genero = dao.generoPorId(id_genero);
+
+        request.setAttribute("genero", genero);
+        request.setAttribute("pageTitle", "Edição de genêro");
+        request.getRequestDispatcher("/jsp/cadastroGenero.jsp").forward(request, response);
+
         } else {
             // Lista Generos
-            request.setAttribute("pageTitle", "Generos");
+            GeneroDAO dao = new GeneroDAO();
+            List<Genero> lista = dao.listar();
+            request.setAttribute("generos", lista);
+            request.setAttribute("pageTitle", "Genêros");
             request.getRequestDispatcher("/jsp/generos.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if("excluir".equals(action)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            GeneroDAO generoDAO = new GeneroDAO();
+
         }
     }
 }
--- src/main/java/estacio/br/dao/GeneroDAO.java
+++ src/main/java/estacio/br/dao/GeneroDAO.java
@@ -28,10 +28,69 @@ public class GeneroDAO {
                 genero.setTitulo(rs.getString("titulo"));
                 generos.add(genero);
             }
-
+            stmt.close();
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return generos;
+    }
+
+    public Genero generoPorId(int id){
+        String sql = "SELECT * FROM generos WHERE id = ?";
+        Genero g = new Genero();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                g.setId(rs.getInt("id"));
+                g.setTitulo(rs.getString("titulo"));
+
+            }
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return g;
+    }
+
+    public boolean editar(Genero genero) throws SQLException {
+        int linhasAfetadas = 0;
+        String sql = "UPDATE generos SET titulo = ?, WHERE id = ?";
+
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, genero.getTitulo());
+            stmt.setInt(3, genero.getId());
+
+            linhasAfetadas = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return linhasAfetadas > 0;
+    }
+
+    public boolean deletar(int id) throws SQLException {
+        int linhasAfetadas = 0;
+        String sql = "DELETE FROM generos WHERE id = ?";
+
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            linhasAfetadas = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return linhasAfetadas > 0;
     }
 }
